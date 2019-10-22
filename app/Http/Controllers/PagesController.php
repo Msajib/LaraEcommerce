2f3c3b46f45f4f5282725274a12c61e85ba87ab2
Md Sajib Mulla <sajib155@gmail.com>
2019-10-23 01:08:02 +0600

    update laravel

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view('index');
+        return view('layout/index');
     }
     public function contact(){
-        return view('contact');
+        return view('layout/contact');
     }
 }
